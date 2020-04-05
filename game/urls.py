a6d19ebabd6f258de766ba7c34de8874b7b0b90f
chrisfelt <chrisfelt.webdev@gmail.com>
2020-04-05 17:30:20 -0600

    user management added

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BalloonPopHomePage
+from .views import BalloonPopHomePage, GameView
 
 urlpatterns = [
-    path('', BalloonPopHomePage.as_view(), name="homepage")
+    path('', BalloonPopHomePage.as_view(), name="homepage"),
+    path('game/', GameView.as_view(), name="game"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class BalloonPopHomePage(TemplateView):
     template_name = "home.html"
+
+
+class GameView(LoginRequiredMixin, TemplateView):
+    login_redirect_field = "account_login"
+    template_name = "game.html"
 
 
 
