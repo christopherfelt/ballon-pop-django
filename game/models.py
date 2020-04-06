c26f8211f4249b8e2651d09ec8e4c9c0c1073f56
chrisfelt <chrisfelt.webdev@gmail.com>
2020-04-06 17:00:55 -0600

    updated game view

--- game/models.py
+++ game/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Score(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    score = models.IntegerField()
+    game_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username + " " + str(self.game_datetime)
--- game/views.py
+++ game/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.db.models import Max
+
+from .models import Score
 
 class BalloonPopHomePage(TemplateView):
     template_name = "home.html"
@@ -10,5 +14,12 @@ class GameView(LoginRequiredMixin, TemplateView):
     login_redirect_field = "account_login"
     template_name = "game.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context['scores'] = Score.objects.filter(user=user).values('score')
+        context['highscore'] = Score.objects.filter(user=user).aggregate(Max('score'))['score__max']
+        return context
+    
 
 
